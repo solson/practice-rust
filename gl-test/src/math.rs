e5d5f95d0db9a8d7eccc2a3a9453729c1a720bd9
Scott Olson <scott@solson.me>
2015-08-02 02:03:37 -0400

    Macroize Vec4 definition and add Vec3.

--- gl-test/src/math.rs
+++ gl-test/src/math.rs
@@ -4,58 +4,61 @@ use std::ops::{Index, IndexMut, Mul};
 
 pub const TAU: GLfloat = 2.0 * PI;
 
-#[derive(Copy, Clone, Debug, PartialEq)]
-pub struct Vec4(pub [GLfloat; 4]);
+macro_rules! define_vec {
+    ($name:ident, $size:expr) => (
+        #[derive(Copy, Clone, Debug, PartialEq)]
+        pub struct $name(pub [GLfloat; $size]);
 
-impl Vec4 {
-    pub fn zero() -> Self {
-        Vec4([0.0, 0.0, 0.0, 0.0])
-    }
+        impl $name {
+            pub fn zero() -> Self {
+                $name([0.0; $size])
+            }
 
-    pub fn x(&self) -> GLfloat { self[0] }
-    pub fn y(&self) -> GLfloat { self[1] }
-    pub fn z(&self) -> GLfloat { self[2] }
-    pub fn w(&self) -> GLfloat { self[3] }
+            /// Calculate the square of the length (or norm) of the vector. Slightly faster than
+            /// `length`.
+            pub fn length_squared(&self) -> GLfloat {
+                let mut sum = 0.0;
 
-    /// Calculate the square of the length (or norm) of the vector. Slightly faster than `length`.
-    pub fn length_squared(&self) -> GLfloat {
-        let mut sum = 0.0;
+                for i in 0..$size {
+                    sum += self[i] * self[i];
+                }
 
-        for i in 0..4 {
-            sum += self[i] * self[i];
+                sum
+            }
+
+            /// Calculate the the length (or norm) of the vector.
+            pub fn length(&self) -> GLfloat {
+                self.length_squared().sqrt()
+            }
+
+            /// Normalize the vector so that it has the same orientation but a length of 1.
+            pub fn normalize(&mut self) {
+                let length = self.length();
+
+                for i in 0..$size {
+                    self[i] /= length;
+                }
+            }
         }
 
-        sum
-    }
+        impl Index<usize> for $name {
+            type Output = GLfloat;
 
-    /// Calculate the the length (or norm) of the vector.
-    pub fn length(&self) -> GLfloat {
-        self.length_squared().sqrt()
-    }
-
-    /// Normalize the vector so that it has the same orientation but a length of 1.
-    pub fn normalize(&mut self) {
-        let length = self.length();
-
-        for i in 0..4 {
-            self[i] /= length;
+            fn index(&self, i: usize) -> &GLfloat {
+                &self.0[i]
+            }
         }
-    }
+
+        impl IndexMut<usize> for $name {
+            fn index_mut(&mut self, i: usize) -> &mut GLfloat {
+                &mut self.0[i]
+            }
+        }
+    );
 }
 
-impl Index<usize> for Vec4 {
-    type Output = GLfloat;
-
-    fn index(&self, i: usize) -> &GLfloat {
-        &self.0[i]
-    }
-}
-
-impl IndexMut<usize> for Vec4 {
-    fn index_mut(&mut self, i: usize) -> &mut GLfloat {
-        &mut self.0[i]
-    }
-}
+define_vec!(Vec3, 3);
+define_vec!(Vec4, 4);
 
 #[derive(Copy, Clone, Debug, PartialEq)]
 pub struct Mat4(pub [[GLfloat; 4]; 4]);

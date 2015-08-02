ee03977836dfc524cddcb7cd7dc4fdfd86c1af77
Scott Olson <scott@solson.me>
2015-08-02 06:52:48 -0400

    Fix camera view matrices (transpose them).

--- gl-test/src/math.rs
+++ gl-test/src/math.rs
@@ -211,10 +211,10 @@ impl Mat4 {
         // Build the rotation/translation matrix that transforms coordinates to the new coordinate
         // system.
         Mat4([
-            [x[0], x[1], x[2], -x.dot(eye)],
-            [y[0], y[1], y[2], -y.dot(eye)],
-            [z[0], z[1], z[2], -z.dot(eye)],
-            [0.0,  0.0,  0.0,   1.0       ],
+            [ x[0],        y[0],        z[0],       0.0],
+            [ x[1],        y[1],        z[1],       0.0],
+            [ x[2],        y[2],        z[2],       0.0],
+            [-x.dot(eye), -y.dot(eye), -z.dot(eye), 1.0],
         ])
     }
 }
